aa4dad7c10765684fa2d0614499cf79537bd7fa1
_iceNuts <billzeng808@gmail.com>
2015-01-08 03:06:42 -0600

    setup network dependency;

---
+++ Carlor/Carlor/objc_bridging_file.h
@@ -0,0 +1,13 @@
+//
+//  objc_bridging_file.h
+//  Carlor
+//
+//  Created by Li Zeng on 1/8/15.
+//  Copyright (c) 2015 CarlorClub. All rights reserved.
+//
+
+#ifndef Carlor_objc_bridging_file_h
+#define Carlor_objc_bridging_file_h
+
+
+#endif
